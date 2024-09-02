fa3d47924f7d1c218fce1c578b9821fec605e9f7
CarlosABCSInsights <163588646+CarlosABCSInsights@users.noreply.github.com>
2024-09-02 14:22:20 -0400

    09-02-2024

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ import torch
 from transformers import AutoTokenizer, AutoModel
 import torch
 import torch.nn.functional as F
-import intel_extension_for_pytorch as ipex
+#import intel_extension_for_pytorch as ipex
 
 import numpy as np
 import pandas as pd
@@ -23,11 +23,11 @@ cors = CORS(app, origins="*")
 #DB Embeddings Connection
 def get_db_connection():
     conn = psycopg2.connect(
-         host="localhost",
+         host="database-1.cn44wueuemjf.us-east-2.rds.amazonaws.com",
          port="5432",
-         database="vector_db2",
+         database="postgres",
          user="postgres",
-         password="Pajam756@"
+         password="bzZP9Hg466swnBZ"
     )
     return conn
 
@@ -85,13 +85,13 @@ def mean_pooling(model_output, attention_mask):
     return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
 tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
-model = AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2').to('xpu')
+model = AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')#.to('xpu')
 
 def encode(text):
     if not text:
         raise ValueError("The input text is empty.")
     encoded_input = tokenizer(
-        text, padding=True, truncation=True, return_tensors='pt').to('xpu')
+        text, padding=True, truncation=True, return_tensors='pt')#.to('xpu')
 
     # Compute token embeddings
     with torch.no_grad():
